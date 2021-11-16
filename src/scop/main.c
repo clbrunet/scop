4d616e601239e63b952de86e4c56f9261ebb0bc5
clbrunet <brunet.clement5@gmail.com>
2021-11-16 14:39:33 +0100

    feat: first triangle

--- src/scop/main.c
+++ src/scop/main.c
@@ -64,6 +64,26 @@ int initialize_gl()
 	return 0;
 }
 
+char *get_file_content(char *path)
+{
+	FILE *file = fopen(path, "rb");
+	if (file == NULL) {
+		return NULL;
+	}
+	fseek(file, 0, SEEK_END);
+	long file_size = ftell(file);
+	char *content = malloc((file_size + 1) * sizeof(char));
+	if (content == NULL) {
+		fclose(file);
+		return NULL;
+	}
+	fseek(file, 0, SEEK_SET);
+	fread(content, sizeof(char), file_size, file);
+	content[file_size] = '\0';
+	fclose(file);
+	return content;
+}
+
 int main(int argc, char *argv[])
 {
 	(void)argc;
@@ -74,16 +94,94 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 	if (initialize_gl() == -1) {
-		return 1;
+		return 2;
 	}
+
+	GLfloat vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f,  0.5f, 0.0f
+	};
+	int vertices_count = 3;
+	int vertices_dimension = 3;
+
+	GLuint vertex_array;
+	glGenVertexArrays(1, &vertex_array);
+	GLuint vertex_buffer;
+	glGenBuffers(1, &vertex_buffer);
+	glBindVertexArray(vertex_array);
+	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+	glBufferData(GL_ARRAY_BUFFER, vertices_count * vertices_dimension * sizeof(GLfloat), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, vertices_dimension, GL_FLOAT, GL_FALSE, vertices_dimension * sizeof(GLfloat), (GLvoid *)(0 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	GLchar *vertex_shader_src = get_file_content("./shaders/vertex.glsl");
+	if (vertex_shader_src == NULL) {
+		glfwTerminate();
+		return 3;
+	}
+	GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex_shader, 1, (const GLchar *const *)&vertex_shader_src, NULL);
+	free(vertex_shader_src);
+	glCompileShader(vertex_shader);
+	GLint success;
+	glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+	if (success == GL_FALSE) {
+		char info_log[512];
+		glGetShaderInfoLog(vertex_shader, 512, NULL, info_log);
+		fprintf(stderr, "Vertex shader compilation error\n%s\n", info_log);
+		glfwTerminate();
+		return 4;
+	}
+
+	GLchar *fragment_shader_src = get_file_content("./shaders/fragment.glsl");
+	if (fragment_shader_src == NULL) {
+		glfwTerminate();
+		return 5;
+	}
+	GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment_shader, 1, (const GLchar *const *)&fragment_shader_src, NULL);
+	free(fragment_shader_src);
+	glCompileShader(fragment_shader);
+	// GLint success;
+	glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+	if (success == GL_FALSE) {
+		char info_log[512];
+		glGetShaderInfoLog(fragment_shader, 512, NULL, info_log);
+		fprintf(stderr, "Fragment shader compilation error\n%s\n", info_log);
+		glfwTerminate();
+		return 6;
+	}
+
+	GLuint program = glCreateProgram();
+	glAttachShader(program, vertex_shader);
+	glAttachShader(program, fragment_shader);
+	glLinkProgram(program);
+	glDeleteShader(vertex_shader);
+	glDeleteShader(fragment_shader);
+	glGetProgramiv(program, GL_LINK_STATUS, &success);
+	if (success == GL_FALSE) {
+		char info_log[512];
+		glGetProgramInfoLog(program, 512, NULL, info_log);
+		fprintf(stderr, "Program link error\n%s\n", info_log);
+		glfwTerminate();
+		return 7;
+	}
+	glUseProgram(program);
 
 	while (glfwWindowShouldClose(window) == GLFW_FALSE) {
 		glClear(GL_COLOR_BUFFER_BIT);
+		glBindVertexArray(vertex_array);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+	glDeleteVertexArrays(1, &vertex_array);
+	glDeleteBuffers(1, &vertex_buffer);
+	glDeleteProgram(program);
 
-	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
 }
